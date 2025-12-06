94f0ea5e1d0fc032d65cf28286064e33f1da7061
Fariha Promi <farihapromi2611@gmail.com>
2025-12-06 19:37:10 +0600

    Add func for crud

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
@@ -10,10 +11,15 @@ import (
 func main() {
 	r := httprouter.New()
 	uc := controllers.NewUserController(getSession())
-	r.GET("")
-	r.POST("")
-	r.DELETE("")
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r) //go server
 }
 func getSession() *mgo.Session {
-
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
